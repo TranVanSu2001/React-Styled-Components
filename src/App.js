110df5087b000ce5fd2c16250ecd9bd247285a2a
TranVanSu2001 <80629567+TranVanSu2001@users.noreply.github.com>
2021-10-03 22:00:31 +0700

    Styled Components

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import "./App.css";
+import Button from "./elements/Button";
+
+const theme = {
+  primary: "teal",
+  secondary: "green",
+  alert: "yellow",
+  font: "sans-serif",
+};
+
+const H1 = styled.h1`
+  font-family: ${(props) => props.theme.font};
+`;
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <H1>Style Component</H1>
+        <form action="">
+          <input type="text" />
+          <button>Create</button>
+          <Button color="secondary">Create</Button>
+        </form>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
